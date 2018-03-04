4bda3fb89e357164c6eb44eb6e1bdd75098e51d9
rachakondaharish <rachakondaharish90@gmail.com>
2018-03-04 12:36:20 -0600

    added client request entity

---
+++ src/main/java/com/cvs/vaccine/clinic/scheduler/clinic_schedule/entities/ClientRequestEntity.java
@@ -0,0 +1,28 @@
+package com.cvs.vaccine.clinic.scheduler.clinic_schedule.entities;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class ClientRequestEntity {
+	public String clinic_name;
+	public String contact_full_name;
+	public long phone_number_1;
+	public long phone_number_2;
+	public long phone_number_3;
+	public String email;
+	public String address_link_1;
+	public String address_link_2;
+	public String city;
+	public int zip_code;
+	public String state;
+	public Date clinic_date;
+	public Time clinic_start_time;
+	public Time end_time;
+	public int participants_between_ages_4_64;
+	public int participants_aged_65_and_up;
+	public String use_a_voucher;
+	
+	
+	
+
+}
